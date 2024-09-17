24b3ca234f409a18d7c6761d382f1d46e2f361f1
Prashant Kumar <148891940+Prashant18GR@users.noreply.github.com>
2024-09-17 19:49:17 +0000

    let_const_var_ki _kahani

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14430
+let accountEmail ="Kohli30@gmail.com"
+var accountPassword = "54321"
+accountCity= "Bengaluru"
+let accountstate;
+
+accountEmail="Kohli40@gmail.com"
+accountPassword="202021"
+accountCity="Delhi"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+
+/*prefere not to use var
+bcuz of issue in scope and block scope and functional scope
+
+
+*/
+//accountId =5
+console.log(accountId) 
