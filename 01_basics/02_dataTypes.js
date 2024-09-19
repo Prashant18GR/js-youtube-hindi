8ab2f03687cab961baa9d61ec5509441d3c7c43b
Prashant Kumar <148891940+Prashant18GR@users.noreply.github.com>
2024-09-19 19:18:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict ";// treat all code of JS as newer version
+console.log("Prashant")
+console.log(5+6)
+let age=25;
+let isloggedin= false
+let state; //undefined
+let city= null // here null is an object
+console.log(typeof "Prashant")
+console.log(typeof age)
+console.log(state)
+console.log(typeof city)
